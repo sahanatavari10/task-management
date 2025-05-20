ace93c8b3adc1fbc0f575c883a96fca5a0b1a28e
Sahana Tavari <sahana.jagadeeshtavari@externos-uy.cencosud.com>
2025-05-20 23:15:00 +0530

    feat: implemented register and login apis

---
+++ routes/auth_routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
+from models import User, db
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'])
+    new_user = User(username=data['username'], email=data['email'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(message="User registered successfully"), 201
+
+@auth_bp('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if user and check_password_hash(user.password, data['password']):
+        token = create_access_token(identity=user.id)
+        return jsonify(token=token)
+    return jsonify(message="Invalid credentials"), 401
